a3470e4ca8a553131816221fece2cdbc9868b086
无风引漩 <zyf199966@126.com>
2019-02-18 11:33:41 +0800

    C控制台工程存在大量路径问题，暂时用python掩耳盗铃

--- ball/main.c
+++ ball/main.c
@@ -1,87 +1,114 @@
 #include "darknet.h"
 
-void test_ball(char *cfgfile, char *weightfile, char *filename);
-
-int main(){
-    char cfg[100], weights[100], filename[100];
-    printf("usage: [cfg] [weights] [filename]\n");
-    scanf("%s%s%s", &cfg, &weights, &filename);
-    if(!cfg || !weights || !filename){
-        printf("usage: [cfg] [weights] [filename]\n");
-        return 1;
-    }
-    test_ball(cfg, weights, filename);
-
-    return 0;
-}
-/*
-void run_ball(int argc, char **argv)
+void test_ball(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh, float hier_thresh, char *outfile, int fullscreen)
 {
-    if (argc < 4 || strcmp(argv[2], "test"))
-    {
-        fprintf(stderr, "usage: %s %s test [cfg] [weights (optional)]\n", argv[0], argv[1]);
-        return;
-    }
+    list *options = read_data_cfg(datacfg);
+    char *name_list = option_find_str(options, "names", "data/names.list");
+    char **names = get_labels(name_list);
 
-    char *cfg = argv[3];
-    char *weights = (argc > 4) ? argv[4] : 0;
-    char *filename = (argc > 5) ? argv[5] : 0;
-
-    test_writing(cfg, weights, filename);
-}
-*/
-void test_ball(char *cfgfile, char *weightfile, char *filename)
-{
-    network *net = parse_network_cfg(cfgfile);
-    if (weightfile)
-    {
-        load_weights(net, weightfile);
-    }
+    image **alphabet = load_alphabet();
+    network *net = load_network(cfgfile, weightfile, 0);
     set_batch_network(net, 1);
     srand(2222222);
-    clock_t time;
+    double time;
     char buff[256];
     char *input = buff;
-    while (1)
-    {
-        if (filename)
-        {
+    float nms=.45;
+    while(1){
+        if(filename){
             strncpy(input, filename, 256);
-        }
-        else
-        {
+        } else {
             printf("Enter Image Path: ");
             fflush(stdout);
             input = fgets(input, 256, stdin);
-            if (!input)
-                return;
+            if(!input) return;
             strtok(input, "\n");
         }
+        image im = load_image_color(input,0,0);
+        image sized = letterbox_image(im, net->w, net->h);
+        layer l = net->layers[net->n-1];
 
-        image im = load_image_color(input, 0, 0);
-        resize_network(net, im.w, im.h);
-        printf("%d %d %d\n", im.h, im.w, im.c);
-        float *X = im.data;
-        time = clock();
+
+        float *X = sized.data;
+        time=what_time_is_it_now();
         network_predict(net, X);
-        printf("%s: Predicted in %f seconds.\n", input, sec(clock() - time));
-        image pred = get_network_image(net);
+        printf("%s: Predicted in %f seconds.\n", input, what_time_is_it_now()-time);
+        int nboxes = 0;
+        detection *dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, 0, 1, &nboxes);
 
-        image upsampled = resize_image(pred, im.w, im.h);
-        image thresh = threshold_image(upsampled, .5);
-        pred = thresh;
-
-        show_image(pred, "prediction", 1000);
-        show_image(im, "orig", 1000);
+        if (nms) do_nms_sort(dets, nboxes, l.classes, nms);
+        draw_detections(im, dets, nboxes, thresh, names, alphabet, l.classes);
+        free_detections(dets, nboxes);
+        if(outfile){
+            save_image(im, outfile);
+        }
+        else{
+            save_image(im, "predictions");
 #ifdef OPENCV
-        cvWaitKey(0);
-        cvDestroyAllWindows();
+            make_window("predictions", 512, 512, 0);
+            show_image(im, "predictions", 0);
 #endif
+        }
 
-        free_image(upsampled);
-        free_image(thresh);
         free_image(im);
-        if (filename)
-            break;
+        free_image(sized);
+        if (filename) break;
+    }
+}
+
+void run_ball(int argc, char **argv)
+{
+    char *prefix = find_char_arg(argc, argv, "-prefix", 0);
+    float thresh = find_float_arg(argc, argv, "-thresh", .5);
+    float hier_thresh = find_float_arg(argc, argv, "-hier", .5);
+    int cam_index = find_int_arg(argc, argv, "-c", 0);
+    int frame_skip = find_int_arg(argc, argv, "-s", 0);
+    int avg = find_int_arg(argc, argv, "-avg", 3);
+    if(argc < 4){
+        fprintf(stderr, "usage: %s %s [train/test/valid] [cfg] [weights (optional)]\n", argv[0], argv[1]);
+        return;
+    }
+    char *gpu_list = find_char_arg(argc, argv, "-gpus", 0);
+    char *outfile = find_char_arg(argc, argv, "-out", 0);
+    int *gpus = 0;
+    int gpu = 0;
+    int ngpus = 0;
+    if(gpu_list){
+        printf("%s\n", gpu_list);
+        int len = strlen(gpu_list);
+        ngpus = 1;
+        int i;
+        for(i = 0; i < len; ++i){
+            if (gpu_list[i] == ',') ++ngpus;
+        }
+        gpus = calloc(ngpus, sizeof(int));
+        for(i = 0; i < ngpus; ++i){
+            gpus[i] = atoi(gpu_list);
+            gpu_list = strchr(gpu_list, ',')+1;
+        }
+    } else {
+        gpu = gpu_index;
+        gpus = &gpu;
+        ngpus = 1;
+    }
+
+    int clear = find_arg(argc, argv, "-clear");
+    int fullscreen = find_arg(argc, argv, "-fullscreen");
+    int width = find_int_arg(argc, argv, "-w", 0);
+    int height = find_int_arg(argc, argv, "-h", 0);
+    int fps = find_int_arg(argc, argv, "-fps", 0);
+    //int class = find_int_arg(argc, argv, "-class", 0);
+
+    char *datacfg = argv[3];
+    char *cfg = argv[4];
+    char *weights = (argc > 5) ? argv[5] : 0;
+    char *filename = (argc > 6) ? argv[6]: 0;
+    if(0==strcmp(argv[2], "test")) test_ball(datacfg, cfg, weights, filename, thresh, hier_thresh, outfile, fullscreen);
+    else if(0==strcmp(argv[2], "demo")) {
+        list *options = read_data_cfg(datacfg);
+        int classes = option_find_int(options, "classes", 20);
+        char *name_list = option_find_str(options, "names", "data/names.list");
+        char **names = get_labels(name_list);
+        demo(cfg, weights, thresh, cam_index, filename, names, classes, frame_skip, prefix, avg, hier_thresh, width, height, fps, fullscreen);
     }
 }
